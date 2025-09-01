58f2f87cab7a239406b6de87ddad773faf5d7b69
Souhaib Zarai <souhaibzarai18@gmail.com>
2025-09-01 14:59:07 +0100

    Handle routing

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '/core/routes/routes.dart';
+import '/features/login/presentation/login_screen.dart';
+import '/features/onboarding/onboarding_screen.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('No route defined for this path')),
+          ),
+        );
+    }
+  }
+}
---
+++ lib/core/routes/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  const Routes._();
+
+  static const String onBoardingScreen = '/onBoardingScreen';
+  static const String loginScreen = '/loginScreen';
+  
+}
