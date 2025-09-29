801fb646df54d4524d681fa64319aaad59e352c8
Souhaib Zarai <souhaibzarai18@gmail.com>
2025-09-29 17:20:37 +0100

    Setting App bar and blue container for home screen

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/doctors_blue_container.dart';
+import 'widgets/home_app_bar.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Screen')),
-      body: const Center(child: Text('Home Screen!!')),
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const HomeAppBar(), //
+                const DoctorsBlueContainer(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
