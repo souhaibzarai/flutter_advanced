ecad23e15ff09a3e1a99c50dc422f064901e5e2b
Souhaib Zarai <souhaibzarai18@gmail.com>
2025-10-07 01:50:42 +0100

    Doctors ListView refactoring

---
+++ lib/features/home/ui/widgets/doctors_list_view_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '/core/helpers/spacing.dart';
+import '/core/theming/styles.dart';
+import '../../data/models/specializations_response_model.dart';
+
+class DoctorsListViewItem extends StatelessWidget {
+  const DoctorsListViewItem({super.key, required this.doctorsModel});
+
+  final DoctorsModel? doctorsModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(12),
+            child: Image.asset(
+              'assets/images/doctor-speciality-image.png',
+              width: 120,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+          ),
+          horizontalSpace(16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  doctorsModel?.name ?? 'Doctor',
+                  style: TextStyles.font18DarkBlueBold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                verticalSpace(5),
+                Text(
+                  '${doctorsModel?.degree} | ${doctorsModel?.phone}',
+                  style: TextStyles.font12SecondaryMedium,
+                ),
+                verticalSpace(5),
+                Text(
+                  doctorsModel?.email ?? 'Email',
+                  style: TextStyles.font12SecondaryMedium,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
