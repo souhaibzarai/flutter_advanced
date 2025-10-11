2728b4dd46cb84c1ca56478adf487557c627008d
Souhaib Zarai <souhaibzarai18@gmail.com>
2025-10-11 16:27:17 +0100

    Specializations & Doctors Ui

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 import '/core/helpers/spacing.dart';
+import 'widgets/doctors_list/doctors_bloc_builder.dart';
 import 'widgets/doctors_blue_container.dart';
 import 'widgets/doctors_speciality_see_all.dart';
 import 'widgets/home_app_bar.dart';
-import 'widgets/specializations_and_doctors_bloc_builder.dart';
+import 'widgets/specializations_list/specializations_bloc_builder.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -22,8 +23,10 @@ class HomeScreen extends StatelessWidget {
               const DoctorsBlueContainer(),
               verticalSpace(24),
               const DoctorsSpecialitySeeAll(),
-              verticalSpace(18),
-              const SpecializationsAndDoctorsBlocBuilder(),
+              verticalSpace(16),
+              const SpecializationsBlocBuilder(),
+              verticalSpace(8),
+              const DoctorsBlocBuilder(),
             ],
           ),
         ),
